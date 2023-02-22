1dd970281e124880f0f26055eedd255148b55ecb
Wipfli, Lorenzo (STUDENTS) <lw13k049@campus.unibe.ch>
2023-02-22 20:03:56 +0100

    Change latitude longitude types due to changed Oracle approach.

--- contracts/RoomBooking.sol
+++ contracts/RoomBooking.sol
@@ -1,14 +1,10 @@
 pragma solidity ^0.8.9;
 
 struct Room {
-    /*
-    * Both values are intended to have 15 decimal points.
-    * longitude must be between 180'00000'00000'00000 and -180'00000'00000'00000
-    * latitude  must be between  90'00000'00000'00000 and  -90'00000'00000'00000
-    *
-    */
-    int128 latitude;
-    int128 longitude;
+
+    string latitude;
+    string longitude;
+    uint searchRadius;
     bool bookable;
     uint pricePerDay;
     string uri;
