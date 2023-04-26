7f8d9d55f470785965f3eb9257aa819c6746f336
Lorenzo Wipfli <lorenzo.wipfli@students.unibe.ch>
2023-04-26 13:38:05 +0200

    Refactor OracleMock to be beased on MockOracle from Chainlink contracts.

--- contracts/OracleMock.sol
+++ contracts/OracleMock.sol
@@ -1,32 +1,7 @@
-pragma solidity ^0.8.9;
+pragma solidity ^0.6.7;
 
-// Adapted from https://github.com/smartcontractkit/hardhat-starter-kit/blob/main/contracts/test/MockOracle.sol for hihgher solidity version.
-//import "@chainlink/contracts/src/v0.6/tests/MockOracle.sol";
-//import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenReceiver.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/ChainlinkRequestInterface.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
-
-//import "@chainlink/contracts/src/v0.8/vendor/SafeMathChainlink.sol";
-
-contract OracleMock  is ChainlinkRequestInterface {
-
-    function oracleRequest(
-    address sender,
-    uint256 requestPrice,
-    bytes32 serviceAgreementID,
-    address callbackAddress,
-    bytes4 callbackFunctionId,
-    uint256 nonce,
-    uint256 dataVersion,
-    bytes calldata data
-  ) external {}
-
-  function cancelOracleRequest(
-    bytes32 requestId,
-    uint256 payment,
-    bytes4 callbackFunctionId,
-    uint256 expiration
-  ) external{}
-
-
+// Use https://github.com/smartcontractkit/hardhat-starter-kit/blob/main/contracts/test/MockOracle.sol as basis for oracle mocking.
+import "@chainlink/contracts/src/v0.6/tests/MockOracle.sol";
+contract OracleMock is MockOracle {
+constructor(address _link) MockOracle(_link) public { }
 }
