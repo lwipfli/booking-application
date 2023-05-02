50c941ccf5c6e04d7f15b57bffd0ce5056b7dcbd
Wipfli, Lorenzo (STUDENTS) <lw13k049@campus.unibe.ch>
2023-05-02 20:48:51 +0200

    Begin Proxy implementation with storage contract.

---
+++ contracts/upgradable/BookingUpgradeable.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BookingProxy is UUPSUpgradeable, Ownable {
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
+}
